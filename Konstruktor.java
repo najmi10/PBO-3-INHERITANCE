04a12e629e7682e471f435c7c8353c7b2b9017ce
najmi10 <53500211+najmi10@users.noreply.github.com>
2019-10-29 16:19:52 +0700

    Add files via upload

---
+++ Konstruktor.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lat_2;
+
+/**
+ *
+ * @author ica
+ */
+public class Konstruktor {
+    public static void main(String[] args) {
+        Employ  programmer1 = new Employ("123456778", "Yanto", 32);
+        programmer1.info();
+    }
+}
---
+++ Pertama.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lat_1;
+
+/**
+ *
+ * @author ica
+ */
+public class Pertama {
+    private int a = 10;
+    protected void terpotek(){
+        System.out.println("Method ini hanya untuk anaknya");
+    }
+    
+    public void info(){
+        System.out.println("a = " + a);
+        System.out.println("Dipanggil pada = " + this.getClass().getName());
+    }
+    
+    
+}
